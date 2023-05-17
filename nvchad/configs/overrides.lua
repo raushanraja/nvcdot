cdae799df6765318e344062b7bbde22380d4d1e9
Raushan <vraushanraja@gmail.com>
2023-05-18 00:29:09 +0530

    Update overrides.lua
    - Add rust
    - Add which key override

--- nvchad/configs/overrides.lua
+++ nvchad/configs/overrides.lua
@@ -31,6 +31,8 @@ M.mason = {
     "typescript-language-server",
     "deno",
     "pyright",
+    "rust_analyzer",
+    "rustfmt",
   },
 }
 
@@ -76,5 +78,18 @@ M.nvterm = {
     auto_insert = true,
   },
 }
+
+M.whichkwy = {
+      presets = {
+      operators = false, -- adds help for operators like d, y, ...
+      motions = true, -- adds help for motions
+      text_objects = true, -- help for text objects triggered after entering an operator
+      windows = true, -- default bindings on <c-w>
+      nav = true, -- misc bindings to work with windows
+      z = true, -- bindings for folds, spelling and others prefixed with z
+      g = true, -- bindings for prefixed with g
+    },
+}
+
 
 return M
